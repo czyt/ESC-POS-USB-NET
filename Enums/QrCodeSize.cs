9924b985283207832667e978f90596798cbb8c58
虫子樱桃 <czyt@w.cn>
2020-12-03 14:46:03 +0800

    添加项目文件。

---
+++ Enums/QrCodeSize.cs
@@ -0,0 +1,24 @@
+﻿namespace ESC_POS_USB_NET.Enums
+{
+    public enum QrCodeSize
+    {
+        Size0,
+        Size1,
+        Size2,
+        Size3,
+        Size4,
+        Size5,
+        Size6,
+        Size7,
+        Size8,
+        Size9,
+        Size10,
+        Size11,
+        Size12,
+        Size13
+
+
+
+    }
+}
+
